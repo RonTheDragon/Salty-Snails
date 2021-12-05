ec78f53c53dfd40a35131a67c0d62b805be05779
RonTheDragon <60644954+RonTheDragon@users.noreply.github.com>
2021-12-05 19:40:18 +0200

    Beautiful world

---
+++ Assets/SaltySnails/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Joystick joystick;
+    GameObject Snail;
+    CharacterController CC;
+    public float Speed = 10;
+    public float RotateSpeed = 20;
+    public float Gravity = 10;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Snail = transform.GetChild(0).gameObject;
+        CC = Snail.GetComponent<CharacterController>();
+        anim = Snail.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    void Movement()
+    { 
+        
+        if (joystick.Vertical != 0) { CC.Move(Snail.transform.forward * Speed * joystick.Vertical * Time.deltaTime); anim.SetBool("Walk", true); }
+        else { anim.SetBool("Walk", false); }
+
+        if (joystick.Horizontal != 0) { Snail.transform.rotation *=  Quaternion.Euler(0, joystick.Horizontal* RotateSpeed * Time.deltaTime, 0); }
+
+        if (!CC.isGrounded) { CC.Move(Vector3.down * 10 * Time.deltaTime); }
+    }
+}
